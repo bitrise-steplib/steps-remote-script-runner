717ac580d295fe64f833e3dd4f0cb747f5a07085
Viktor Benei <viktor.benei@gmail.com>
2014-09-17 17:02:22 +0200

    simplified

--- step.sh
+++ step.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
 echo
-echo "DOWNLOAD_SCRIPT_URL: $DOWNLOAD_SCRIPT_URL"
+echo "DOWNLOAD_SCRIPT_URL: ${DOWNLOAD_SCRIPT_URL}"
 
 # Required input validation
-# API key
-if [[ ! $DOWNLOAD_SCRIPT_URL ]]; then
+if [[ ! ${DOWNLOAD_SCRIPT_URL} ]]; then
 	echo
     echo "No DOWNLOAD_SCRIPT_URL provided as environment variable. Terminating..."
     echo
     exit 1
 fi
 
-######################
-
-echo
-echo "--Saving script to file--"
-echo
-
-curl -o remotescript.sh "$DOWNLOAD_SCRIPT_URL"
 
 echo
 echo "--Executing script--"
 echo
 
-bash ./remotescript.sh
+curl -sSL "${DOWNLOAD_SCRIPT_URL}" | bash
