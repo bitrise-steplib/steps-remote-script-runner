8544b92edae46883d64eb67f644d3e102ab14a25
Viktor Benei <viktor.benei@gmail.com>
2014-09-18 11:24:56 +0200

    debug log style change

--- step.sh
+++ step.sh
@@ -14,17 +14,17 @@ fi
 
 echo
 echo "---------------------------------------------------"
-echo "---------- Executing script: ${DOWNLOAD_SCRIPT_URL}"
+echo "--- Executing script: ${DOWNLOAD_SCRIPT_URL}"
 echo
 
 curl -sSL "${DOWNLOAD_SCRIPT_URL}" | bash
 if [ $? -ne 0 ] ; then
-	echo "---------- [!] The script returned with a non success code!"
+	echo "--- [!] The script returned with a non success code!"
 	echo "---------------------------------------------------"
 	exit 1
 fi
 
 echo
-echo "---------- Script returned with a success code - OK"
+echo "--- Script returned with a success code - OK"
 echo "---------------------------------------------------"
 exit 0
